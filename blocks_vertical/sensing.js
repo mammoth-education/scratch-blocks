1707e78df1960366bdc80caef7412790e19816d8
Mac <mac@MacdeMBP.(none)>
2023-05-22 09:58:54 +0800

    feat: 修改侦测中的按下block块移动端只有方向选择

--- blocks_vertical/sensing.js
+++ blocks_vertical/sensing.js
@@ -218,56 +218,70 @@ Blockly.Blocks['sensing_keyoptions'] = {
    * @this Blockly.Block
    */
   init: function() {
+    var SENSING_KEYPRESSED = [
+      [Blockly.Msg.EVENT_WHENKEYPRESSED_SPACE, 'space'],
+      [Blockly.Msg.EVENT_WHENKEYPRESSED_UP, 'up arrow'],
+      [Blockly.Msg.EVENT_WHENKEYPRESSED_DOWN, 'down arrow'],
+      [Blockly.Msg.EVENT_WHENKEYPRESSED_RIGHT, 'right arrow'],
+      [Blockly.Msg.EVENT_WHENKEYPRESSED_LEFT, 'left arrow'],
+      [Blockly.Msg.EVENT_WHENKEYPRESSED_ANY, 'any'],
+      ['a', 'a'],
+      ['b', 'b'],
+      ['c', 'c'],
+      ['d', 'd'],
+      ['e', 'e'],
+      ['f', 'f'],
+      ['g', 'g'],
+      ['h', 'h'],
+      ['i', 'i'],
+      ['j', 'j'],
+      ['k', 'k'],
+      ['l', 'l'],
+      ['m', 'm'],
+      ['n', 'n'],
+      ['o', 'o'],
+      ['p', 'p'],
+      ['q', 'q'],
+      ['r', 'r'],
+      ['s', 's'],
+      ['t', 't'],
+      ['u', 'u'],
+      ['v', 'v'],
+      ['w', 'w'],
+      ['x', 'x'],
+      ['y', 'y'],
+      ['z', 'z'],
+      ['0', '0'],
+      ['1', '1'],
+      ['2', '2'],
+      ['3', '3'],
+      ['4', '4'],
+      ['5', '5'],
+      ['6', '6'],
+      ['7', '7'],
+      ['8', '8'],
+      ['9', '9']
+    ]
+    
+    var MOBILE_SENSING_KEYPRESSED = [
+      [Blockly.Msg.EVENT_WHENKEYPRESSED_UP, 'up arrow'],
+      [Blockly.Msg.EVENT_WHENKEYPRESSED_DOWN, 'down arrow'],
+      [Blockly.Msg.EVENT_WHENKEYPRESSED_RIGHT, 'right arrow'],
+      [Blockly.Msg.EVENT_WHENKEYPRESSED_LEFT, 'left arrow'],
+    ]
+
+    var isMobile = false;
+    if (window.cordova && (window.cordova.platformId === 'ios' || window.cordova.platformId === 'android') ||
+        navigator.userAgent.indexOf('Mobile') > -1) {
+        isMobile = true;
+    }
     this.jsonInit({
       "message0": "%1",
       "args0": [
         {
           "type": "field_dropdown",
           "name": "KEY_OPTION",
-          "options": [
-            [Blockly.Msg.EVENT_WHENKEYPRESSED_SPACE, 'space'],
-            [Blockly.Msg.EVENT_WHENKEYPRESSED_UP, 'up arrow'],
-            [Blockly.Msg.EVENT_WHENKEYPRESSED_DOWN, 'down arrow'],
-            [Blockly.Msg.EVENT_WHENKEYPRESSED_RIGHT, 'right arrow'],
-            [Blockly.Msg.EVENT_WHENKEYPRESSED_LEFT, 'left arrow'],
-            [Blockly.Msg.EVENT_WHENKEYPRESSED_ANY, 'any'],
-            ['a', 'a'],
-            ['b', 'b'],
-            ['c', 'c'],
-            ['d', 'd'],
-            ['e', 'e'],
-            ['f', 'f'],
-            ['g', 'g'],
-            ['h', 'h'],
-            ['i', 'i'],
-            ['j', 'j'],
-            ['k', 'k'],
-            ['l', 'l'],
-            ['m', 'm'],
-            ['n', 'n'],
-            ['o', 'o'],
-            ['p', 'p'],
-            ['q', 'q'],
-            ['r', 'r'],
-            ['s', 's'],
-            ['t', 't'],
-            ['u', 'u'],
-            ['v', 'v'],
-            ['w', 'w'],
-            ['x', 'x'],
-            ['y', 'y'],
-            ['z', 'z'],
-            ['0', '0'],
-            ['1', '1'],
-            ['2', '2'],
-            ['3', '3'],
-            ['4', '4'],
-            ['5', '5'],
-            ['6', '6'],
-            ['7', '7'],
-            ['8', '8'],
-            ['9', '9']
-          ]
+          "options": isMobile ? MOBILE_SENSING_KEYPRESSED : SENSING_KEYPRESSED
         }
       ],
       "extensions": ["colours_sensing", "output_string"]
